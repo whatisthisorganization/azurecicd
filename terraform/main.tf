cf4b3d166711dbeefd92b99d16b45b90d3485dfd
Behroz Ahmad Ali <behroz.ahmad_ali@dhl.com>
2025-01-08 13:20:22 +0100

    create storage account for state file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "terraform-backend-rg"
+    storage_account_name = "tfststrgaccountgithubtest"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
+
 provider "azurerm" {
   features {}
 
