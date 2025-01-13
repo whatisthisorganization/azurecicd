fc6de62149305d301a18985a08e673d3f33a027c
Behroz Ahmad Ali <behroz.ahmad_ali@dhl.com>
2025-01-13 16:40:48 +0100

    vars updated on tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,15 +6,36 @@ variable "location" {
   default = "East US"
 }
 
+# variable "subscription_id" {
+#     default = "d55c4594-129f-44a9-ba1e-b49345abdbd8"
+# }
+# variable "client_id" {
+#     default = "7d7670b5-72e3-466f-9ed0-bd520f76c095"
+# }
+# variable "client_secret" {
+#     default = "G2J8Q~qwZzMgPJKA9hUNu9l57wq5D1YV2ujdJctY"
+# }
+# variable "tenant_id" {
+#     default = "cedf37a5-fa53-4c67-96b3-85aba04f990a"
+# }
+
 variable "subscription_id" {
-    default = "d55c4594-129f-44a9-ba1e-b49345abdbd8"
+  description = "Azure Subscription ID"
+  type        = string
 }
+
 variable "client_id" {
-    default = "7d7670b5-72e3-466f-9ed0-bd520f76c095"
+  description = "Azure Client ID"
+  type        = string
 }
+
 variable "client_secret" {
-    default = "G2J8Q~qwZzMgPJKA9hUNu9l57wq5D1YV2ujdJctY"
+  description = "Azure Client Secret"
+  type        = string
+  sensitive   = true
 }
+
 variable "tenant_id" {
-    default = "cedf37a5-fa53-4c67-96b3-85aba04f990a"
+  description = "Azure Tenant ID"
+  type        = string
 }
