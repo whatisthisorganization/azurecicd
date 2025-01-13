1bce366e56ce4bfb2c8660887674c4b7d3ae6e33
Behroz Ahmad Ali <behroz.ahmad_ali@dhl.com>
2025-01-13 16:46:01 +0100

    update vars

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,36 +6,36 @@ variable "location" {
   default = "East US"
 }
 
-# variable "subscription_id" {
-#     default = "d55c4594-129f-44a9-ba1e-b49345abdbd8"
-# }
-# variable "client_id" {
-#     default = "7d7670b5-72e3-466f-9ed0-bd520f76c095"
-# }
-# variable "client_secret" {
-#     default = "G2J8Q~qwZzMgPJKA9hUNu9l57wq5D1YV2ujdJctY"
-# }
-# variable "tenant_id" {
-#     default = "cedf37a5-fa53-4c67-96b3-85aba04f990a"
-# }
-
 variable "subscription_id" {
-  description = "Azure Subscription ID"
-  type        = string
+    default = "4713046b-1fac-4a36-a39b-0413983dc0e6"
 }
-
 variable "client_id" {
-  description = "Azure Client ID"
-  type        = string
+    default = "3dbe2792-7ac9-44e7-a0db-c906c5793077"
 }
-
 variable "client_secret" {
-  description = "Azure Client Secret"
-  type        = string
-  sensitive   = true
+    default = "fle8Q~gt3mxpyLNA6ckPPD0yJzT2EwAR-_m-GdBE"
+}
+variable "tenant_id" {
+    default = "7c35a675-f206-43e5-85bf-7f1eccae0d20"
 }
 
-variable "tenant_id" {
-  description = "Azure Tenant ID"
-  type        = string
-}
+# variable "subscription_id" {
+#   description = "Azure Subscription ID"
+#   type        = string
+# }
+
+# variable "client_id" {
+#   description = "Azure Client ID"
+#   type        = string
+# }
+
+# variable "client_secret" {
+#   description = "Azure Client Secret"
+#   type        = string
+#   sensitive   = true
+# }
+
+# variable "tenant_id" {
+#   description = "Azure Tenant ID"
+#   type        = string
+# }
