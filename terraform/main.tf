35f6d00b483537748c1993918d1b2fcb8efabff0
Behroz Ahmad Ali <behroz.ahmad_ali@dhl.com>
2025-01-13 17:04:23 +0100

    storage account name fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "terraform-backend-rg"
-    storage_account_name = "tfststrgaccountgithubtst"
+    resource_group_name  = "terraform-statefile-rg-dev"
+    storage_account_name = "tfstatefilewesteuropedev"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
@@ -22,7 +22,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "sa" {
-  name                     = "mystrgforgithubtest123"
+  name                     = "mystrgforgithubtest543"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
@@ -30,13 +30,13 @@ resource "azurerm_storage_account" "sa" {
 }
 
 resource "azurerm_cognitive_account" "openai" {
-  name                = "azureaistudiotest"
+  name                = "azureaistudiotest543"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   kind                = "OpenAI"
   sku_name            = "S0"
 
-  custom_subdomain_name = "azureaistudiotest"
+  custom_subdomain_name = "azureaistudiotest543"
   dynamic_throttling_enabled = false
   public_network_access_enabled = true
 
