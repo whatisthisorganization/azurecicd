4ad5ec74a96fe74e1376eba7c269a21230f98bd1
Behroz Ahmad Ali <behroz.ahmad_ali@dhl.com>
2025-01-13 20:07:50 +0100

    subscription id fixed

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,7 +10,7 @@ terraform {
 provider "azurerm" {
   features {}
 
-  subscription_id = var.subscription_id
+  subscription_id = var.SUBSCRIPTION_ID
   client_id       = var.client_id
   client_secret   = var.client_secret
   tenant_id       = var.tenant_id
