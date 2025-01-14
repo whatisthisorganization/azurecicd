323ddaec6662c3c67906fb70764154399d852ef4
Behroz Ahmad Ali <behroz.ahmad_ali@dhl.com>
2025-01-14 11:01:58 +0100

    rm undefined output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,6 +1,6 @@
-output "storage_account_name" {
-  value = azurerm_storage_account.sa.name
-}
+# output "storage_account_name" {
+#   value = azurerm_storage_account.sa.name
+# }
 
 output "azurerm_resource_group" {
   value = azurerm_resource_group.rg.name
